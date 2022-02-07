02f82e6a3e6e6a4f2b1b2b26ef5f13650c9ece8b
Marcio Machado Pontes <marcinhojazz@gmail.com>
2022-02-07 17:52:40 -0300

    created login page

---
+++ src/pages/login.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+import FormArea from '../components/FormArea'
+
+const Login = () => {
+  return (
+    <>
+      <Head>
+        <title>Login - ioasys book</title>
+      </Head>
+      <FormArea />
+    </>
+  )
+}
+
+export default Login
